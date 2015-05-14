d32ffe5c24a501e06e74aa28e55dd328da3bebe7
nestor-01 <Nestor@Nestor-PC.home>
2015-05-14 00:42:55 -0500

    Merge branch 'master' of https://github.com/nestor-01/Genekasoft
    Conflicts:
    	.idea/workspace.xml
    	src/main/java/com/geneka/product/ws/ProductWS.java

--- src/main/java/com/geneka/product/ws/ProductWS.java
+++ src/main/java/com/geneka/product/ws/ProductWS.java
@@ -10,11 +10,7 @@ import java.util.Map;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.geneka.common.util.DefaultContextImpl;
 import com.geneka.common.util.Tools;
