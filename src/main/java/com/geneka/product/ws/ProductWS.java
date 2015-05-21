e7ab0df23e2a80bac5fc9a8a95fa8be1d44ba457
nestor-01 <Nestor@Nestor-PC.home>
2015-05-20 23:23:51 -0500

    Changed name of services and added  service of status

--- src/main/java/com/geneka/product/ws/ProductWS.java
+++ src/main/java/com/geneka/product/ws/ProductWS.java
@@ -175,11 +175,17 @@ public class ProductWS {
         }
 	}
 
-	@RequestMapping(value = "/inactive", method = RequestMethod.GET)
+	@RequestMapping(value = "/changeStatus", method = RequestMethod.GET)
 	public @ResponseBody String inactive(@RequestParam(value="id", required=true) String id,
 										 @RequestParam(value="active", required=true) Boolean active) throws Exception
 	{
 		return productService.inactive(id, active)?"ok":"null";
+	}
+
+	@RequestMapping(value = "/findByWord", method = RequestMethod.GET)
+	public @ResponseBody String findByWord(@RequestParam(value="", required=true) String id) throws Exception
+	{
+		return "";
 	}
 
 }
